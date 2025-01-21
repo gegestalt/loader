c710f879933d54809cae4964d38efefeb29e14ce
gegestalt <guneyunal@sabanciuniv.edu>
2025-01-21 12:49:11 +0300

    load_chunks added.

--- main_laoder.py
+++ main_laoder.py
@@ -1,17 +1,30 @@
 
+import hashlib
+import os
+import sys
+import requests
+
+import tempfile
 
 
 def generate_filename(n):
     h = hashlib.sha256()
     h.update(str(n).encode())
     digest = h.hexdigest()[0x0:0xf]
-    filename = tempfile.NamedTemporaryFile(delete=False, suffix=f'__{digest_}')
+    filename = tempfile.NamedTemporaryFile(delete=False, suffix=f'__{digest}')
     return filename.name
 
 def load_chunks(url):
-
-
-
+    parts = {}
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        for i,chunk  in enumerate(r.iter_content(chunk_size=0x4000),start=0x1):
+            if chunk:
+                filename = generate_filename(len(parts))
+                parts[i] = filename
+                with open(filename, "wb") as fh:
+                    fh.write(chunk)
+    return parts
 
 def rebuild_file(parts):
     new_filename = tempfile.NamedTemporaryFile(delete=False,   suffix=".exe")
